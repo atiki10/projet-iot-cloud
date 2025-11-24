4fc3b94832d97584d1992b042cfbeb7a383af741
yassine el-atiki <atikiyassine10@gmail.com>
2025-11-24 12:20:41 +0100

    fix syntax error in MqttService fin

--- electronic-side/MqttRestApp/src/main/java/com/tracksecure/mqttrestapp/repository/SensorDataRepository.java
+++ electronic-side/MqttRestApp/src/main/java/com/tracksecure/mqttrestapp/repository/SensorDataRepository.java
@@ -4,7 +4,9 @@ import com.tracksecure.mqttrestapp.model.SensorData;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SensorDataRepository extends MongoRepository<SensorData, String> {
-    SensorData findTopByOrderByIdDesc();
+    Optional<SensorData> findTopByOrderByIdDesc();
 }
